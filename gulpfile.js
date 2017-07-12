486ad78ec7d1bdc17990776ef1e603a6e01a460d
Victor Idongesit <victoridongesit@gmail.com>
2017-07-12 10:57:02 +0100

    add define task sequence for installation

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const bower = require('gulp-bower');
 const sass = require('gulp-sass');
 const nodemon = require('gulp-nodemon');
 const jshint = require('gulp-jshint');
-
+const clean = require('gulp-clean');
 
 
 gulp.task('watch', function() {
@@ -54,10 +54,8 @@ gulp.task('nodemon', function () {
 });
 // Bower task
 gulp.task('bower', function() {
-  return bower({
-      verbosity: 2,
-      directory: './public/lib'
-  });
+  return bower()
+  .pipe(gulp.dest('./public/lib'));
 });
 
 // Jshint
@@ -65,11 +63,39 @@ gulp.task('jshint', function() {
   return gulp.src(['gruntfile.js', 'public/js/**/*.js', 'test/**/*.js', 'app/**/*.js'])
     .pipe(jshint());
 });
+
 // Concurrent Tasks
 gulp.task('concurrent', ['watch', 'nodemon']);
+
+
+
+/** Installation Sequence */
+
+
+gulp.task('install', ['clean']);
+
+// Delete bower_components folder
+gulp.task('clean', ['move:route'], function() {
+  return gulp.src('bower_components', { read: false })
+  .pipe(clean());
+});
+// Move route.js in angular-ui-utils
+gulp.task('move:route', ['move:bootstrap'], function() {
+  gulp.src("public/lib/angular-ui-utils/modules/route/**/*.*")
+  .pipe(gulp.dest("public/lib/angular-ui-utils/modules/"));
+});
+
+// Move bootstrap files
+gulp.task('move:bootstrap', ['runbower'], function() {
+  gulp.src("public/lib/bootstrap/dist/**/*.*")
+  .pipe(gulp.dest("public/lib/bootstrap/"));
+});
+
+// Bower task
+gulp.task('runbower', ['bower']);
+
 // Default task(s)
-gulp.task('default', ['jshint', 'sass'] );
+gulp.task('default', ['jshint', 'concurrent', 'sass'] );
+
 // Test task
 gulp.task('test', ['mochaTest']);
-// Bower task
-gulp.task('install', ['bower']);
