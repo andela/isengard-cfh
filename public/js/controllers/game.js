54573ba3b4e4c1a875092c607f47bfd8a35bde15
Victor Idongesit <victoridongesit@gmail.com>
2017-08-23 15:26:33 +0100

    add route to start game

--- public/js/controllers/game.js
+++ public/js/controllers/game.js
@@ -1,5 +1,5 @@
 angular.module('mean.system')
-.controller('GameController', ['$scope', 'game', '$timeout', '$location', '$http', 'MakeAWishFactsService', '$dialog', function ($scope, game, $timeout, $location, $http, MakeAWishFactsService, $dialog) {
+.controller('GameController', ['$scope', 'game', '$timeout', '$location', '$http', 'MakeAWishFactsService', '$dialog', '$window', function ($scope, game, $timeout, $location, $http, MakeAWishFactsService, $dialog, $window) {
     $(document).ready(function() {
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
       $('.modal').modal();
@@ -137,6 +137,7 @@ angular.module('mean.system')
     return game.winningCard !== -1;
   };
 
+<<<<<<< HEAD
   $scope.startGame = function() {
     game.startGame();
   };
@@ -146,6 +147,34 @@ angular.module('mean.system')
     $location.path('/');
   };
 
+=======
+    $scope.startGame = function() {
+      game.startGame();
+      var token = $window.localStorage.getItem('token');
+      var config = { headers: {
+          Authorization: 'Bearer ' + token,
+          Accept: 'application/json;odata=verbose',
+          'X-Testing': 'testing'
+        }
+      };
+      const playersIds = game.players.map((player, index) => {
+        return player.userID;
+      });
+      console.log(playersIds);
+      $http.post(`http://localhost:3000/api/games/${game.gameID}/start`, {
+        playersIds
+      }, config).then((res) => {
+        console.log('Game saved');
+      }, (err) => {
+        console.log(err);
+      });
+    };
+    $scope.abandonGame = function() {
+      game.leaveGame();
+      $location.path('/');
+    };
+    
+>>>>>>> add route to start game
     // Catches changes to round to update when no players pick card
     // (because game.state remains the same)
   $scope.$watch('game.round', function() {
