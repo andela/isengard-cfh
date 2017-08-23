540c8e6ba24639efc0ed424bfd80f8267805ecfe
Victor Idongesit <victoridongesit@gmail.com>
2017-08-23 15:35:46 +0100

    fix merge conflict

--- app/controllers/game.js
+++ app/controllers/game.js
@@ -9,7 +9,11 @@ const Game = mongoose.model('Game');
 const User = mongoose.model('User');
 
 exports.startGame = function (req, res) {
+<<<<<<< HEAD
   const headerBearer = req.headers.authorization || '';
+=======
+  const headerBearer = req.headers.authorization || [];
+>>>>>>> specify status code for server response
   const token = headerBearer.split(' ')[1];
   if (token) {
     jwt.verify(token, config.secret, function(err, decoded) {
@@ -32,6 +36,7 @@ exports.startGame = function (req, res) {
       });
       newGame.save(function(err) {
         if (err) {
+          console.log(err);
           return res.json({
             status: false,
             message: 'Could not save game'
@@ -59,14 +64,14 @@ exports.startGame = function (req, res) {
               }
             }, function(err) {
               if (err) {
-                return res.json({
+                return res.status(400).json({
                   status: false,
                   message: 'Could not add game to user model'
                 });
               }
             });
           });
-          return res.send({ err, users });
+          return res.send({ game: newGame });
         });
       });
     });
@@ -79,12 +84,12 @@ exports.startGame = function (req, res) {
 };
 
 exports.endGame = function(req, res) {
-  const headerBearer = req.headers.authorization;
+  const headerBearer = req.headers.authorization || [];
   const token = headerBearer.split(' ')[1];
   if (token) {
     jwt.verify(token, config.secret, function(err, decoded) {
       if (err) {
-        return res.json({
+        return res.status(401).json({
           status: false,
           message: 'Token not valid'
         });
@@ -96,13 +101,13 @@ exports.endGame = function(req, res) {
         id: gameId,
       }, function(err, game) {
         if (err || game.length === 0) {
-          return res.json({
+          return res.status(404).json({
             status: false,
             message: 'Game not found'
           });
         }
         if (game[0].creatorId !== userId) {
-          return res.json({
+          return res.status(403).json({
             status: false,
             message: 'Unauthorized. Only game creator can update game'
           });
@@ -113,7 +118,7 @@ exports.endGame = function(req, res) {
           }
         }, function(err) {
           if (err) {
-            return res.json({
+            return res.status(400).json({
               status: false,
               message: 'Could not update game info',
               error: err
@@ -127,7 +132,7 @@ exports.endGame = function(req, res) {
       });
     });
   } else {
-    res.json({
+    res.status(403).json({
       status: false,
       message: 'No token provided'
     });
